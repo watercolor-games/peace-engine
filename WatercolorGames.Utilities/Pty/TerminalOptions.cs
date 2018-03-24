8c1a7da0d2ecc777affa3b0dc50fd48d365f9205
The Fuzzy Riolu <theplexgate@gmail.com>
2018-03-24 17:36:08 -0400

    Remove Peacenet stuff

---
+++ WatercolorGames.Utilities/Pty/TerminalOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace Plex.Objects.Pty
+{
+    /// <summary>
+    /// A class containing settings for a <see cref="PseudoTerminal"/> master/slave pair. 
+    /// </summary>
+    public class TerminalOptions
+    {
+#pragma warning disable CS1591
+        public uint OFlag
+        {
+            set;
+            get;
+        }
+
+        public uint LFlag
+        {
+            set;
+            get;
+        }
+
+        public readonly byte[] C_cc = new byte[20];
+    }
+}
