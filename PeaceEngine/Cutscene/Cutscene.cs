a9fc9f32725bdc3b484a01ffcf29c96bde1ee58a
The Fuzzy Riolu <theplexgate@gmail.com>
2018-04-06 22:58:34 -0400

    stuff

--- PeaceEngine/Cutscene/Cutscene.cs
+++ PeaceEngine/Cutscene/Cutscene.cs
@@ -3,6 +3,8 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Plex.Engine.GraphicsSubsystem;
 using Plex.Engine.Interfaces;
+using Plex.Engine.TextRenderers;
+using Plex.Engine.Themes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,9 @@ namespace Plex.Engine.Cutscene
     /// </summary>
     public abstract class Cutscene : EntityContainer, ILoadable
     {
+        [Dependency]
+        private ThemeManager _theme = null;
+
         [Dependency]
         private CutsceneManager _cutscene = null;
 
@@ -57,6 +62,32 @@ namespace Plex.Engine.Cutscene
         /// Fire a cutscene play event.
         /// </summary>
         public virtual void OnPlay() { }
-        
+
+        public override void Draw(GameTime time, GraphicsContext gfx)
+        {
+            string placeholder = "Placeholder";
+            string desc = $"Coded cutscene: {Name}";
+
+            var header = _theme.Theme.GetFont(TextFontStyle.Header1);
+            var highlight = _theme.Theme.GetFont(TextFontStyle.Highlight);
+
+            var headColor = _theme.Theme.GetFontColor(TextFontStyle.Header1);
+            var highlightColor = _theme.Theme.GetFontColor(TextFontStyle.Highlight);
+
+
+            var pMeasure = header.MeasureString(placeholder);
+            var dMeasure = highlight.MeasureString(desc);
+
+            float centerY = (gfx.Height - (pMeasure.Y + 10 + dMeasure.Y)) / 2;
+
+            gfx.BeginDraw();
+
+            _theme.Theme.DrawControlBG(gfx, 0, 0, gfx.Width, gfx.Height);
+
+            gfx.DrawString(placeholder, new Vector2((gfx.Width - pMeasure.X) / 2, centerY), headColor, header, TextAlignment.Left, int.MaxValue, WrapMode.None);
+            gfx.DrawString(desc, new Vector2((gfx.Width - dMeasure.X) / 2, centerY + pMeasure.Y + 10), highlightColor, highlight, TextAlignment.Left, int.MaxValue, WrapMode.None);
+
+            gfx.EndDraw();
+        }
     }
 }
