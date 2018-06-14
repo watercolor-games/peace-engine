55f40ce2f55da4252bf073a2e8793a3860071cc0
The Fuzzy Riolu <theplexgate@gmail.com>
2018-06-14 19:49:46 -0400

    Clear the branch

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# All you had to do was build the damn solution, CJ.
-target=${1:-Release} # The engine runs a lot better in Release mode. Use it by default.
-xbuild -property:Configuration=$target
--- updatepackages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Download nuget packages on linux
-
-mkdir -p packages
-cd packages
-for fname in $(find .. -iname "packages.config")
-do
-	nuget install "$fname"
-done
